49bc3a890c98ea62b5400864093587e8bc0bec4c
Shaun Lazaro <shaunlazaro@gmail.com>
2018-12-02 21:01:46 -0500

    Setup input parsing and testing scenes
    UI layout and multicontroller input are implemented
    Title screen layout is also implemented, but not functional

---
+++ FightingGame/Assets/Scripts/Input/SpecialInput.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpecialInput{
+
+    public string[] buttons;
+    private int currentIndex = 0; //moves along the array as buttons are pressed
+
+    public float allowedTimeBetweenButtons = 0.3f; //tweak as needed
+    private float timeLastButtonPressed;
+
+    private int stickNum = 1;
+    // 0: Keyboard
+    // 1,2,3...: Joystick Numbers
+
+    // Create a specialinput object using a string array
+    public SpecialInput(string[] b)
+    {
+        buttons = b;
+    }
+
+
+
+    // Second constructor which allows multiple controllers
+    public SpecialInput(string[] b, int joyStickNum)
+    {
+        buttons = b;
+        stickNum = joyStickNum;
+    }
+
+    //Call in update, does something when 
+    public bool Check()
+    {
+        if (Time.time > timeLastButtonPressed + allowedTimeBetweenButtons) currentIndex = 0;
+        {
+            if (currentIndex < buttons.Length)
+            {
+                if ((buttons[currentIndex] == "down" && Down()) ||
+                (buttons[currentIndex] == "up" && Up()) ||
+                (buttons[currentIndex] == "left" && Left()) ||
+                (buttons[currentIndex] == "right" && Right()) ||
+                (buttons[currentIndex] != "down" && buttons[currentIndex] != "up" && buttons[currentIndex] != "left"
+                    && buttons[currentIndex] != "right" && Input.GetButtonDown(buttons[currentIndex] + "P" + stickNum)))
+                {
+                    timeLastButtonPressed = Time.time;
+                    currentIndex++;
+                }
+
+                if (currentIndex >= buttons.Length)
+                {
+                    currentIndex = 0;
+                    return true;
+                }
+                else
+                    return false;
+            }
+        }
+
+        return false;
+    }
+
+    private bool Down()
+    {
+        return Input.GetAxisRaw("Vertical" + "P" + stickNum) == -1;
+    }
+    private bool Up()
+    {
+        return Input.GetAxisRaw("Vertical" + "P" + stickNum) == 1;
+    }
+    private bool Left()
+    {
+        return Input.GetAxisRaw("Horizontal" + "P" + stickNum) == -1;
+    }
+    private bool Right()
+    {
+        return Input.GetAxisRaw("Horizontal" + "P" + stickNum) == 1;
+    }
+}
---
+++ FightingGame/Assets/Scripts/Input/TestInput.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestInput : MonoBehaviour {
+
+    public string[] specialAttackInput;
+    private SpecialInput specialAttack;
+
+    public int controllerNumber;
+
+    void Start()
+    {
+        specialAttack = new SpecialInput(specialAttackInput,controllerNumber);
+    }
+
+    void Update()
+    {
+        if (specialAttack.Check())
+            Debug.Log("Special Attack Input Read!");
+        else if (Input.GetButtonDown("LowPunchP"+controllerNumber))
+            Debug.Log("LowPunch!");
+        else if (Input.GetButtonDown("LowKickP"+controllerNumber))
+            Debug.Log("LowKick!");
+        else if (Input.GetButtonDown("HighKickP"+controllerNumber))
+            Debug.Log("HighKick!");
+        else if (Input.GetButtonDown("HighPunchP"+controllerNumber))
+            Debug.Log("HighPunch!");
+        else
+            Debug.Log("Rip!");
+    }
+}
